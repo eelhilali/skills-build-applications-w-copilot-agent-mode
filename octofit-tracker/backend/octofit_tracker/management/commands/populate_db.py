b08a235d867af0e9a974a81c2776153cc32a9609
EL_Houssaine <eelhilali@users.noreply.github.com>
2025-04-10 22:20:34 +0000

    Add additional test data to populate_db.py script for OctoFit Tracker

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -2,6 +2,7 @@ import json
 from datetime import timedelta
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from bson import ObjectId
 
 class Command(BaseCommand):
     help = 'Populate the database with test data'
@@ -39,5 +40,38 @@ class Command(BaseCommand):
         # Populate workouts
         for workout_data in data['workouts']:
             Workout.objects.get_or_create(**workout_data)
+
+        # Additional test data for users
+        additional_users = [
+            User(_id=ObjectId(), username='ironman', email='ironman@mhigh.edu', password='ironmanpassword'),
+            User(_id=ObjectId(), username='blackwidow', email='blackwidow@mhigh.edu', password='blackwidowpassword')
+        ]
+        User.objects.bulk_create(additional_users)
+
+        # Additional test data for teams
+        additional_team = Team(_id=ObjectId(), name='Red Team')
+        additional_team.save()
+        additional_team.members.add(*additional_users)
+
+        # Additional test data for activities
+        additional_activities = [
+            Activity(_id=ObjectId(), user=additional_users[0], activity_type='Yoga', duration=timedelta(minutes=45)),
+            Activity(_id=ObjectId(), user=additional_users[1], activity_type='Pilates', duration=timedelta(minutes=30))
+        ]
+        Activity.objects.bulk_create(additional_activities)
+
+        # Additional test data for leaderboard
+        additional_leaderboard_entries = [
+            Leaderboard(_id=ObjectId(), user=additional_users[0], score=110),
+            Leaderboard(_id=ObjectId(), user=additional_users[1], score=120)
+        ]
+        Leaderboard.objects.bulk_create(additional_leaderboard_entries)
+
+        # Additional test data for workouts
+        additional_workouts = [
+            Workout(_id=ObjectId(), name='Yoga Session', description='A calming yoga session for flexibility and relaxation'),
+            Workout(_id=ObjectId(), name='Pilates Class', description='A core-strengthening pilates class')
+        ]
+        Workout.objects.bulk_create(additional_workouts)
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data'))
